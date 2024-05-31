cc66fa233f74902c22255f9c562c2cdbdff21504
Joshua Z <joshuazamora0310@hotmail.com>
2024-05-31 13:19:06 -0700

    Add project images to Projects page

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def projects():
             'description': 'A real-time energy monitoring application developed to track the energy production of solar pavers.',
             'technologies': 'Python, Flask, HTML, CSS, JavaScript, Bootstrap',
             'link': 'https://github.com/yocomplex/solar_paver_monitor',
-            'image': 'project1.jpg'
+            'image': 'project3.jpg'
         },
         {
             'name': 'Mario Kart Simulation',
@@ -32,7 +32,7 @@ def projects():
             'description': 'A scanner and parser for an esoteric programming language.',
             'technologies': 'C++, Compilers, OOP',
             'link': 'https://github.com/yocomplex/scannerAndParser',
-            'image': 'project3.png'
+            'image': 'project1.png'
         }
     ]
     return render_template('projects.html', projects=projects)
