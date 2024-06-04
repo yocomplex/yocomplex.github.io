a694cfefa5ba4b7ea5b9d615623b29ace1eb4fb8
Joshua Z <joshuazamora0310@hotmail.com>
2024-06-04 08:02:35 -0700

    bug fixes 1.6

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -12,33 +12,33 @@ document.addEventListener('DOMContentLoaded', () => {
         count = (count + 1) % texts.length;
     }, 2000);
 
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
+    // document.getElementById('contactForm').addEventListener('submit', function(event) {
+    //     event.preventDefault(); // Prevent the default form submission
 
-        const formData = new FormData(this);
-        const data = {
-            'name': formData.get('name'),
-            'email': formData.get('email'),
-            'message': formData.get('message')
-        };
+    //     const formData = new FormData(this);
+    //     const data = {
+    //         'name': formData.get('name'),
+    //         'email': formData.get('email'),
+    //         'message': formData.get('message')
+    //     };
 
-        fetch('https://script.google.com/macros/s/AKfycbzgsgRAz7rvN4oq_rytg_1XRwFR5UuBXV3ZpZdsvKtP1Z_EwO_X5seFChLZEVTv4bJC/exec', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams(data).toString()
-        })
-        .then(response => response.json())
-        .then(response => {
-            document.getElementById('responseMessage').innerText = response.result === 'success' ? 'Thank you for your message!' : 'There was an error submitting your message. Please try again.';
-            if (response.result === 'success') {
-                this.reset();
-            }
-        })
-        .catch(error => {
-            document.getElementById('responseMessage').innerText = 'There was an error submitting your message. Please try again.';
-            console.error('Error:', error);
-        });
-    });
+    //     fetch('https://script.google.com/macros/s/AKfycbzgsgRAz7rvN4oq_rytg_1XRwFR5UuBXV3ZpZdsvKtP1Z_EwO_X5seFChLZEVTv4bJC/exec', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/x-www-form-urlencoded',
+    //         },
+    //         body: new URLSearchParams(data).toString()
+    //     })
+    //     .then(response => response.json())
+    //     .then(response => {
+    //         document.getElementById('responseMessage').innerText = response.result === 'success' ? 'Thank you for your message!' : 'There was an error submitting your message. Please try again.';
+    //         if (response.result === 'success') {
+    //             this.reset();
+    //         }
+    //     })
+    //     .catch(error => {
+    //         document.getElementById('responseMessage').innerText = 'There was an error submitting your message. Please try again.';
+    //         console.error('Error:', error);
+    //     });
+    // });
 });
